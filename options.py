5064adc512b052863097552cae31d74c579a6410
JonathanOll <jonathan.ol01100@gmail.com>
2024-07-14 10:48:23 +0200

    restructuration

--- options.py
+++ options.py
@@ -1,37 +1,44 @@
-import os
-import sys
-from json import loads, dumps
+from command import *
+
 
 options = {}
 
-def options_exists(prog_dir):
-    return os.path.exists(prog_dir + "/options.json")
+class OptionCommand(Command):
+    def __init__(self, prog_dir):
+        super().__init__(prog_dir)
+        self.name = "options"
+        self.aliases = ["options", "opt", "o", "settings", "option", "setting"]
 
-def init_options(prog_dir):
-    open(prog_dir + "/options.json", "w+").write("{}")
+    def run(self, args):
+        super().run(args)
 
-def verif_options():
-    if "open_vscode" not in options : options["open_vscode"] = True
-    if "connect_to_git" not in options : options["connect_to_git"] = True
-    if "git_main_branch" not in options : options["git_main_branch"] = "main"
+    def options_exists(self):
+        return os.path.exists(self.prog_dir + "/options.json")
 
-def save_options(prog_dir):
-    open(prog_dir + "/options.json", "w").write(dumps(options))
+    def init_options(self):
+        open(self.prog_dir + "/options.json", "w+").write("{}")
 
-def load_options(prog_dir):
-    try:
-        s = open(prog_dir + "/options.json", "r").read()
-        data = loads(s)
-        for k, v in data.items():
-            options[k] = v
-        return True
-    except: return False
+    def verif_options(self):
+        if "open_vscode" not in options : options["open_vscode"] = True
+        if "connect_to_git" not in options : options["connect_to_git"] = True
+        if "git_main_branch" not in options : options["git_main_branch"] = "main"
 
+    def save_options(self):
+        open(self.prog_dir + "/options.json", "w").write(dumps(options))
 
-def show_options():
-    print("Options :")
-    for k, v in options.items():
-        print("\t- " + str(k) + " : " + str(v))
+    def load_options(self):
+        try:
+            s = open(self.prog_dir + "/options.json", "r").read()
+            data = loads(s)
+            for k, v in data.items():
+                options[k] = v
+            return True
+        except: return False
+
+    def show_options(self):
+        print("Options :")
+        for k, v in options.items():
+            print("\t- " + str(k) + " : " + str(v))
 
 
 
